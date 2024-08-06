317f644c234c379561f4de3dd2a26d2c83c648a9
YYF <1760945581@qq.com>
2024-08-06 14:49:26 +0800

    实现交互功能 添加像素字体 添加场景0、场景1 添加TextMeshPro 添加Ruletile

--- Assets/Scripts/GameModel.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GameModel : MonoBehaviour
-{
-    public PlayerController playerController;
-    public InputController inputController;
-}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.U2D;
@@ -13,8 +14,11 @@ public class PlayerController : MonoBehaviour
     [Tooltip("冲刺推力")]
     public float dashForce = 5f;
 
+    public float smoothTime = 1f;
+    public Vector2 currentVelocity;
+
     public int jumpCount = 2;
-    public int dashCount = 2;
+    public int dashCount = 1;
 
     new Rigidbody2D rigidbody2D;
     public Vector2 moveDirection;
@@ -36,24 +40,37 @@ public class PlayerController : MonoBehaviour
     void MoveDirection()
     {
         if (Input.GetKey(KeyCode.A))
-            moveDirection = Vector2.left;
+            moveDirection.x = -1;
         else if (Input.GetKey(KeyCode.D))
-            moveDirection = Vector2.right;
+            moveDirection.x = 1;
         else if (Input.GetKey(KeyCode.W))
-            moveDirection = Vector2.up;
+            moveDirection.y = 1;
         else if (Input.GetKey(KeyCode.S))
-            moveDirection = Vector2.down;
+            moveDirection.y = -1;
         else
             moveDirection = Vector2.zero;
+
+        moveDirection.Normalize();
     }
 
     void CharacterMovement()
     {
-        if (moveDirection.x != 0)
-        {
-            transform.Translate(moveDirection * speed * Time.deltaTime);
-        }
+        // 当前物体的位置
+        Vector2 currentPosition = transform.position;
+        Vector2 targetPosition = currentPosition + moveDirection * speed;
+        targetPosition.y=currentPosition.y;
 
+        // 平滑移动到目标位置
+        Vector2 newPosition = Vector2.SmoothDamp(
+            currentPosition,
+            targetPosition,
+            ref currentVelocity,
+            smoothTime
+        );
+
+        // 更新物体位置
+        transform.position = new Vector3(newPosition.x, newPosition.y, transform.position.z);
+        
         if (jumpCount>0 && Input.GetKeyDown(KeyCode.W))
         {
             rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
@@ -70,13 +87,18 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void OnCollisionStay2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Ground"))
         {
-            jumpCount = 2;
-            dashCount = 2;
+            ResetAbilityCount();
         }
+    }
+
+    void ResetAbilityCount()
+    {
+        jumpCount = 1;
+        dashCount = 0;
     }
     
 
