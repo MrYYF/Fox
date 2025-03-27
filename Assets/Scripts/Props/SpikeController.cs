27dfab34516a832f53bd7ddc808394a3e08b0532
YYF <1760945581@qq.com>
2025-03-27 21:43:56 +0800

    2025.3.27 - 心态崩了，画了那么久的地图没保存，unity崩溃了

--- Assets/Scripts/Props/SpikeController.cs
+++ Assets/Scripts/Props/SpikeController.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
 public class SpikeController : MonoBehaviour
 {
+    
     void OnTriggerStay2D(Collider2D collision)
     {
         collision.GetComponent<Character>()?.TakeDamage(1);
-        collision.GetComponent<PlayerController>()?.GetHurt(transform);
+        collision.GetComponent<PlayerController>()?.GetHurt(collision.transform);
         collision.GetComponent<PlayerAnimation>()?.Hurt();
     }
 }
