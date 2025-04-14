6fde6ffaef1d13505dfc29e551fb3cf419670bf4
YYF <1760945581@qq.com>
2025-04-14 20:54:28 +0800

    丰富跳跃功能 - 实现土狼跳 - 实现跳跃预输入 - 实现小跳和大跳

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,11 +1,11 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 /// <summary>
 /// 玩家控制类
 /// </summary>
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     #region 
     /**
      * 变量 Variables
@@ -17,12 +17,16 @@ public class PlayerController : MonoBehaviour
     [Tooltip("滑铲速度修正")] public float slideSpeed;
     [Tooltip("滑铲推力")] public float slideForce;
     [Tooltip("跳跃推力")] public float jumpForce;
+    [Tooltip("持续跳跃推力")] public float holdJumpForce;
+    [Tooltip("持续跳跃推力时间")] public float holdJumpTime; // 持续跳跃时间
+    float holdJumpCounter; // 持续跳跃计时器
     [Tooltip("土狼跳时间")] public float coyoteTime;
     float coyoteTimeCounter; //土狼跳计时器
     [Tooltip("跳跃预输入时间")] public float jumpBufferTime; // 跳跃预输入时间
     float jumpBufferCounter; // 跳跃预输入计时器
-    [Tooltip("冲刺推力")] public float dashForce;
     
+    [Tooltip("冲刺推力")] public float dashForce;
+
 
     /**
      * 组件
@@ -36,7 +40,7 @@ public class PlayerController : MonoBehaviour
     /**
      * 状态
      */
-    //public bool isJump;
+    [HideInInspector] public bool isJump;
     [HideInInspector] public bool isCrouch;
     [HideInInspector] public bool isHurt;
 
@@ -44,66 +48,57 @@ public class PlayerController : MonoBehaviour
 
     #region 生命周期函数
     //初始化组件
-    void Awake()
-    {
+    void Awake() {
+        GameManager.Instance.RigisterPlayer(gameObject); //注册玩家到游戏管理器
+
         rb = GetComponent<Rigidbody2D>();
         physicsCheck = GetComponent<PhysicsCheck>();
-        //col = GetComponent<CapsuleCollider2D>();
         playerInputControl = new PlayerInputControl();
-        playerInputControl.Gameplay.Jump.started += Jump;
-        //playerInputControl.Gameplay.Jump.canceled += Bounce;
+        playerInputControl.Gameplay.Jump.started += StartJump;
+        playerInputControl.Gameplay.Jump.canceled += CancelJump;
         playerInputControl.Gameplay.Dash.started += Dash;
     }
-    void OnEnable()
-    {
+
+    void OnEnable() {
         playerInputControl?.Enable();
     }
-    void OnDisable()
-    {
+    void OnDisable() {
         playerInputControl?.Disable();
     }
-    void Start()
-    {
+    void Start() {
         currentSpeed = normalSpeed;
         coyoteTimeCounter = coyoteTime;
     }
-    void Update()
-    {
+    void Update() {
         if (isHurt) return;
         inputDirection = playerInputControl.Gameplay.Move.ReadValue<Vector2>(); //获取输入移动向量
 
         // 土狼跳逻辑
-        if (physicsCheck.isGround)
-        {
+        if (physicsCheck.isGround) 
             coyoteTimeCounter = coyoteTime;
-        }
-        else
-        {
+        else 
             coyoteTimeCounter -= Time.deltaTime;
-        }
+        
 
         // 跳跃预输入逻辑
-        if (jumpBufferCounter > 0)
-        {
+        if (jumpBufferCounter > 0) {
             jumpBufferCounter -= Time.deltaTime;
-            if (physicsCheck.isGround || coyoteTimeCounter > 0)
-            {
+            if (physicsCheck.isGround || coyoteTimeCounter > 0) {
                 PerformJump();
                 jumpBufferCounter = 0;
             }
         }
     }
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
         if (isHurt) return;
+        if (isJump) HoldJump();
         Move();
     }
     #endregion
 
     #region 角色移动相关代码
     //实现平滑移动
-    void Move()
-    {
+    void Move() {
         //平滑移动
         Vector2 targetPosition = rb.position + inputDirection * currentSpeed; //目标位置
         Vector2 currentVelocity = rb.velocity;
@@ -113,26 +108,31 @@ public class PlayerController : MonoBehaviour
         if (currentVelocity.x < 0) transform.localScale = new Vector3(-1, 1, 1);
         if (currentVelocity.x > 0) transform.localScale = new Vector3(1, 1, 1);
     }
-    //重置跳跃输入
-    void Jump(InputAction.CallbackContext context)
-    {
+    //输入跳跃
+    void StartJump(InputAction.CallbackContext context) {
+        isJump = true;
         jumpBufferCounter = jumpBufferTime;
     }
+    //取消跳跃
+    void CancelJump(InputAction.CallbackContext context) {
+        isJump = false;
+        holdJumpCounter = 0; // 重置跳跃持续时间计时器
+    }
     //执行跳跃
-    void PerformJump()
-    {
+    void PerformJump() {
         rb.velocity = new Vector2(rb.velocity.x, 0);
         rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        holdJumpCounter = holdJumpTime; // 初始化跳跃持续时间计时器
     }
-    //TODO:小跳
-    //void Bounce(InputAction.CallbackContext context)
-    //{
-    //    rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.6f);
-    //}
-
+    //长按跳跃跳得更高
+    void HoldJump() {
+        if(holdJumpCounter > 0) {
+            rb.AddForce(Vector2.up * holdJumpForce, ForceMode2D.Force);
+            holdJumpCounter -= Time.fixedDeltaTime; // 减少跳跃持续时间
+        }
+    }
     //冲刺
-    void Dash(InputAction.CallbackContext context)
-    {
+    void Dash(InputAction.CallbackContext context) {
         rb.AddForce(inputDirection * dashForce, ForceMode2D.Impulse);
     }
     //TODO:蹲伏优化
@@ -156,9 +156,11 @@ public class PlayerController : MonoBehaviour
     //}
     #endregion
 
+
+
+
     //受伤弹开
-    public void GetHurt(Transform attacker)
-    {
+    public void GetHurt(Transform attacker) {
         isHurt = true;
         // TODO: 根据接触点弹开
         rb.velocity = Vector2.zero;
