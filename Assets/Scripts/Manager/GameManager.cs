87b23dc18161d2e08d1abd55bdee12a9b0ed22db
YYF <1760945581@qq.com>
2025-04-14 20:55:10 +0800

    添加组件 - 添加GameManager组件 - 实现单例模式

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,29 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    private CinemachineVirtualCamera virtualCamera;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void RigisterPlayer(GameObject player)
+    {
+        virtualCamera = FindObjectOfType<CinemachineVirtualCamera>();
+        if (virtualCamera != null)
+        {
+            virtualCamera.Follow = player.transform;
+            virtualCamera.LookAt = player.transform;
+        }
+        else
+        {
+            Debug.LogError("CinemachineVirtualCamera not found in the scene.");
+        }
+    }
+}
