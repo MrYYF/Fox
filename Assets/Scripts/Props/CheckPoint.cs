d77314dbc926a6a8635524cb693574dde53fc282
YYF <1760945581@qq.com>
2025-06-10 23:57:06 +0800

    addressable资源场景管理 - 添加addressable组件

--- Assets/Scripts/Props/CheckPoint.cs
+++ Assets/Scripts/Props/CheckPoint.cs
@@ -3,26 +3,34 @@ using UnityEngine;
 /**
  * 挂载到存档点，用于调用存档点的相关方法
  */
-public class CheckPoint : MonoBehaviour
-{
-    [Tooltip("重生位置")]public Transform spawnPoint;
-    [Tooltip("触发区域")]public Collider2D checkPointArea;
-    [Tooltip("检查点是否已使用")]public bool isUsed;
+[RequireComponent(typeof(BoxCollider2D))]
+public class CheckPoint : MonoBehaviour {
+    [Tooltip("重生位置")] public Transform spawnPoint;
+    [Tooltip("检查点是否已使用")] public bool isUsed;
+    [Tooltip("是否初始检查点")] public bool isInitialCheckPoint;
 
-    private bool isStayInCheckPointArea; //是否处于触发区域
+    private Collider2D checkPointArea; // 存档点触发器区域
+
+    private void Awake() {
+        checkPointArea = checkPointArea.GetComponent<Collider2D>();
+        checkPointArea.isTrigger = true;// 确保触发器区域是触发器
+    }
+
+    private void Start() {
+        if (isInitialCheckPoint) {
+            SaveManager.Instance.SetCheckPoint(this); // 如果是初始检查点，设置为当前存档点
+        }
+    }
 
 
     //调用SaveManager，将当前存档点设置为当前存档点
-    public void SetCheckPoint() {
-        if (isStayInCheckPointArea) {
-            SaveManager.Instance.SetCheckPoint(this);
-        }
+    public void SetThisCheckPoint() {
+        SaveManager.Instance.SetCheckPoint(this);
     }
-    
+
     private void OnTriggerEnter2D(Collider2D collision) {
-        if(collision.CompareTag("Player") && !isUsed) {
-            isStayInCheckPointArea = true;
-            SetCheckPoint();
+        if (collision.CompareTag("Player") && !isUsed) {
+            SetThisCheckPoint();
             //TODO: UI显示
             //TODO: 粒子效果播放
             //TODO: 音效播放
@@ -31,7 +39,6 @@ public class CheckPoint : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision) {
         if (collision.CompareTag("Player")) {
-            isStayInCheckPointArea = false;
             //TODO: UI隐藏
         }
     }
